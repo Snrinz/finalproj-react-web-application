29ff095df0dbcdf6c699e7248d6582256d2a0406
Phornpailin Khonthad <pilyn24@hotmail.com>
2020-04-15 23:43:59 +0700

    use the movie database api

--- client/src/Trending.js
+++ client/src/Trending.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react'
-// import MovieCard from './MovieCard'
+import React, { useState, useEffect, Component } from 'react'
+import axios from 'axios';
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons"
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
 
 //library for MovieCard
 import { NavLink } from 'react-router-dom'
@@ -13,36 +12,59 @@ import imgMovie from './img/Frozen-2.jpg'
 
 
 export default class Trending extends Component {
+    state = {
+        trend_list: []
+    }
+
+    componentDidMount () {
+        //  axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_MM_KEY}&language=en-US&page=1`)
+        axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=72c7a3ed944673d07bbf1b9b44dc7894&language=en-US&page=1`)
+        .then(res => {
+            // console.log(res.data.results)
+            this.setState({trend_list: res.data.results})
+        })
+        .catch(err => console.log(err))
+        
+    }
     render() {
         return (
             <div style={{display: 'flex', alignItems: 'center'}}>
+
                 <FontAwesomeIcon id="left-right" icon={faAngleLeft} />
                 <div className="trend-movie-section">
-                    <MovieCard></MovieCard>
-                    <MovieCard></MovieCard>
-                    <MovieCard></MovieCard>
-                    <MovieCard></MovieCard>
-
+                    {
+                        <React.Fragment>
+                            {
+                                this.state.trend_list.map(movie => (
+                                    <MovieCard movie={movie} />
+                                ))
+                            }
+                        </React.Fragment>
+                    }
                 </div> 
                 <FontAwesomeIcon id="left-right" icon={faAngleRight} />
-                          
+                        
             </div>
 
         )
     }
+
 }
 
-const MovieCard = () => {
+const MovieCard = (props) => {
+    const { movie } = props
     return (            
         <div className="movie-card">
             <NavLink to="/detail-movie/frozen2" className="img-wrapper">
-                <img id="movie-image" src={imgMovie} alt="sth"></img>
+                <img id="movie-image" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} 
+                alt={movie.poster_path}></img>
             </NavLink>
             
             {/* <FontAwesomeIcon id="star" icon={faStar} /> 
             <p style={{display: 'inline'}} id="rank">10.0</p> */}
-            <NavLink to="/detail-movie/frozen2" className="name-movie">
-                <strong>Frozen 2</strong>
+
+            <NavLink to={`/detail-movie/${movie.id}`} className="name-movie">
+                <strong>{movie.title}</strong>
             </NavLink>
 
             <WatchListButton />        
@@ -57,4 +79,3 @@ const WatchListButton = () => {
         </div>
     )
 }
- 
--- server/index.js
+++ server/index.js
@@ -1,26 +1,45 @@
-// import express from 'express'
-
-// let app = express();
-// app.get('/', (req, res) => {
-//     res.end("Hello World")
-// });
-
-// app.listen(4000, () =>
-//     console.log("Server start at port 4000")  
-// );
-
-
 // index.js -- server side
-import expres from 'express'
-const app = express();
+import express from 'express'; 
+import bodyParser from 'body-parser';
+const app = express(); 
 const port = 4000;
 
-// entry route for oot / request
-app.get('/', (req, res) => {
-    res.end("Hello World");
-})
+app.use(bodyParser.json());
 
-// make server start listening on a specificed port
+// make server start listening on a specified port 
 app.listen(port, () => 
     console.log(`Server started at port ${port}`)
-)
+);
+
+// entry route for root / request
+app.get('/', (req, res) => { 
+    res.end("Hello World"); 
+ })
+
+const Products = [ 
+    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football', sku:'1234'}, 
+    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball', sku:'3444'},  
+    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball', sku:'1344'},  
+    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch', sku:'3422'},  
+    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5', sku:'2567'},  
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7', sku:'3214'},  
+    {category: 'Kitchenware', price: '$9.99', stocked: true, name: 'Pot', sku:'1414'},
+];
+
+// entry route for /product post-request
+app.post('/product', (req, res) => {
+    console.log(req.body);
+    if (req.is('json')) {
+        let body = req.body;
+        Products.push(body);
+        res.end(`Received new product!\n${Object.keys(req.body)[0]}: ${Object.values(req.body)[0]}`);
+    } 
+    else {
+        res.status(400).end(`Expected JSON product data!\n${req.body}`);
+    }
+});
+
+// entry route for /products request 
+app.get('/products', (req, res) => { 
+    res.json(Products); 
+});
